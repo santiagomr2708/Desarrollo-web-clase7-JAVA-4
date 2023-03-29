9aba5187ee01c80445931817448d492ca6c27025
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2023-03-29 11:55:35 -0500

    Add files via upload

---
+++ clase7-java-4/module2.js
@@ -0,0 +1,6 @@
+//export default significa que por defecto la puedo llamar en otro modulo
+export const nombre = 'Juan Pérez';
+
+export const suma=(n1,n2)=>{
+    return n1+n2;
+}
---
+++ clase7-java-4/modules.js
@@ -0,0 +1,22 @@
+//Módulos en JavaScript
+import { nombre, suma } from './module2.js';
+
+console.log(nombre);
+console.log(suma(5,3));
+
+/*
+crear minimo 2 modulos
+-el primer modulo debe de tener:
+*una clase que ereda de otra
+*un objeto que dentro de sus propiedades tenga otro objeto
+
+-el segundo modulo debe tener:
+*importacón de la clase y el objeto
+funcion tipo flecha que haga uso de la clase y el objeto importado
+
+oros adicionales:
+-Tercer modulo que contenga
+*una funcion tipo flecha, que manipule arreglos con los metodos de arreglos
+*Debe tener como parametros de entrada el arreglo y una callback function
+*debe poderse exportar
+*/
