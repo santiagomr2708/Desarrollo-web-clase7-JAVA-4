f214fcd8334aa56befb579da3b9ad52d91feb01d
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2023-03-30 23:35:35 -0500

    Update modules.js

--- clase7-java-4/modules.js
+++ clase7-java-4/modules.js
@@ -9,11 +9,47 @@ crear minimo 2 modulos
 -el primer modulo debe de tener:
 *una clase que ereda de otra
 *un objeto que dentro de sus propiedades tenga otro objeto
+*/
 
--el segundo modulo debe tener:
-*importacón de la clase y el objeto
-funcion tipo flecha que haga uso de la clase y el objeto importado
+class Autos{
+    constructor(marca,año,modelo){
+    this.marca = marca;
+    this.año = año;
+    this.modelo = modelo;
+    }
 
+    modeloA(){
+        return` El auto ${this.marca} es un: ${this.modelo}`;
+    }
+}
+
+
+class JDM extends Autos{
+
+    // constructor de la clase supra
+
+    constructor(marca,año,modelo,motor){
+        super(marca,año,modelo)
+        this.motor = motor;
+    }
+}
+
+//Objeto con otro objeto dentro
+
+const ciudad ={
+    nombre : "Tokyo",
+    existe : true,
+    CaracteristicasH :{
+        Población : "alta",
+        autos: 'bastantes',
+        autopistas: true,
+        carreras: false,
+    },
+}
+
+export{JDM,ciudad}
+
+/*
 oros adicionales:
 -Tercer modulo que contenga
 *una funcion tipo flecha, que manipule arreglos con los metodos de arreglos
