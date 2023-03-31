db0d1ef6c1c20e93baeb5512196dafe880939e84
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2023-03-30 23:50:23 -0500

    Update module2.js

--- clase7-java-4/module2.js
+++ clase7-java-4/module2.js
@@ -4,3 +4,31 @@ export const nombre = 'Juan Pérez';
 export const suma=(n1,n2)=>{
     return n1+n2;
 }
+
+/*
+-el segundo modulo debe tener:
+*importacón de la clase y el objeto
+funcion tipo flecha que haga uso de la clase y el objeto importado
+*/
+
+import { JDM, ciudad } from "./modules.js";
+
+const autos = () => {
+
+    let supra = new tipoA('Toyota', 1993, 'Supra MK4', 'Diesel')
+
+    return supra;
+
+}
+
+console.log(autos());
+
+const Imprimir_ciudad = () => {
+    return `La ciudad ingresada es: ${ciudad.nombre} Existe: ${ciudad.existe}
+Población: ${ciudad.Caracteristicas_ciudad.Población}
+Autos: ${ciudad.Caracteristicas_ciudad.Autos}
+Autopistas: ${ciudad.Caracteristicas_ciudad.Autopistas}
+Carreras_legales: ${ciudad.Caracteristicas_ciudad.Carreras_legales}`
+}
+
+console.log(Imprimir_ciudad());
